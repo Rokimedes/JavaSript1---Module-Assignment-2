c2401e7f7618a496ddf66ec05eb169730eb30af4
Rokimedes <36521512+Rokimedes@users.noreply.github.com>
2018-11-19 08:48:30 +0100

    Revert "Revert "Update javascript_ma2.txt""
    This reverts commit b0ac636e750def51f4219f59f9e4f406d933f233.

--- javascript1_ma2.js
+++ javascript1_ma2.js
@@ -14,7 +14,18 @@ myArray.splice(9, 1); //Starts at the 9th in the Array and removes 1.
 
 
 //4. Given the following paragraph, create a JavaScript function that changes all mentions of strawberry to banana and strawberries to bananas: 
-Strawberries are a popular part of spring and summer diets throughout America. Mouths water from coast to coast each spring, when small white blossoms start to appear on strawberry bushes. They announce the impending arrival of the ruby red berries that so many people crave. Ripe strawberries taste sweet and have only a slight hint of tartness. They are also one of the healthiest fruits around. There are countless recipes for the luscious red berry, but many people prefer to eat them fresh and unaccompanied.
+
+function replaceWords() {
+    var currentString = "Strawberries are a popular part of spring and summer diets throughout America. Mouths water from coast to coast each spring, when small white blossoms start to appear on strawberry bushes. They announce the impending arrival of the ruby red berries that so many people crave. Ripe strawberries taste sweet and have only a slight hint of tartness. They are also one of the healthiest fruits around. There are countless recipes for the luscious red berry, but many people prefer to eat them fresh and unaccompanied.";
+    var banana = currentString.replace(/strawberry/ig, "banana");
+    var bananas = banana.replace(/strawberries/ig, "Bananas");
+    
+    console.log(bananas);
+}
+
+replaceWords();
+
+
 
 
 //5. Create an array and a button. The array should contain 4 football clubs names. When the button is clicked, remove all elements out of the array that we just created. There after add in 4 names of cars inside that array. Log it to the console.
