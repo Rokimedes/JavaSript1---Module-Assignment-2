169743b26798c5dd5e89441ec5397961cf9c2fb3
Rokimedes <36521512+Rokimedes@users.noreply.github.com>
2018-11-19 15:19:01 +0100

    Update javascript1_ma2.js
    Made changes to the Task 5 and moved out the current array's that are about to be removed on click.

--- javascript1_ma2.js
+++ javascript1_ma2.js
@@ -48,15 +48,16 @@ replaceWords();
 
 //5. Create an array and a button. The array should contain 4 football clubs names. When the button is clicked, remove all elements out of the array that we just created. There after add in 4 names of cars inside that array. Log it to the console.
 
-function nameArray() {
     var itemNames = ["Man Utd", "Liverpool", "Man City", "Arsenal"];
     console.log(itemNames);
+
+function nameArray() {
 
     itemNames.splice(0,4);
     itemNames.push("VW", "BMW", "Volvo", "Opel");
 
     console.log(itemNames);
-}
+};
 
 
 //6. Create an array of objects with 3 people inside it. Use the filter function to map by a name.
